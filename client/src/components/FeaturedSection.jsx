81777c7c9b82af57161f57a5766dba870759e02a
Yadri Amzal <justyamzal@gmail.com>
2026-02-16 22:16:37 +0700

    movie card added

--- client/src/components/FeaturedSection.jsx
+++ client/src/components/FeaturedSection.jsx
@@ -2,6 +2,8 @@ import { ArrowRight } from 'lucide-react'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import BlurCircle from './BlurCircle';
+import MovieCard from './MovieCard';
+import { dummyShowsData } from '../assets/assets';
 
 const FeaturedSection = () => {
  const navigate = useNavigate();
@@ -16,7 +18,12 @@ const FeaturedSection = () => {
             </button>
         </div>
 
-        <div></div>
+        {/* Displaying data  */}
+        <div className='flex flex-wrap max-sm:justify-center gap-8 mt-8'>
+            {dummyShowsData.slice(0,4).map((show)=>(
+              <MovieCard key={show._id} movie={show}/>
+            ))}
+        </div>
         {/* Show More Button */}
         <div className='flex justify-center mt-20'>
             <button className='px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer'>Show More</button>
