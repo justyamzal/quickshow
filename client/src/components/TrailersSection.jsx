3555e86488b29fdd300682464ea2fdab44d74606
Yadri Amzal <justyamzal@gmail.com>
2026-02-20 16:41:09 +0700

    rollback to react@18.3.1,react-dom@18.3.1 react-player@2.16.0 and also vite@^6.3.5 for fixing reactplayer issue that not compatible

--- client/src/components/TrailersSection.jsx
+++ client/src/components/TrailersSection.jsx
@@ -14,7 +14,13 @@ const TrailersSection = () => {
 
       <div className='relative mt-6'>
         <BlurCircle top='-100px' right='-100px'/>
-        <ReactPlayer url={currentTrailer.videoUrl} controls={false} className="mx-auto max-w-full" width="960px" height="540px"/>
+        <ReactPlayer 
+  url={currentTrailer?.videoUrl} 
+  controls={true}   // ← change this
+  className="mx-auto max-w-full" 
+  width="960px" 
+  height="540px"
+/>
       </div>
       
       <div className='group grid grid-cols-4 gap-4 md:gap-8 mt-8 max-w-3xl mx-auto'>
