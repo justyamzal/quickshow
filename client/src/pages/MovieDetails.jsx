4782950005f3e8c35c0b4cdf6543107639853a63
Yadri Amzal <justyamzal@gmail.com>
2026-02-23 02:33:04 +0700

    version 2 movie details

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -9,7 +9,7 @@ const MovieDetails = () => {
   const {id} = useParams();
   const [show, setShow] = useState(null);
 
-  const getShow = async()=>{
+  const getShow = async ()=>{
     const show = dummyShowsData.find(show => show._id === id)
     setShow({
       movie: show,
